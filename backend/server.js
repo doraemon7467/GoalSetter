edcce2e09cf2c9c4921242aee60b2d01045a01ed
Aayu <106157824+doraemon7467@users.noreply.github.com>
2024-01-06 18:15:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({credentials:true,origin: 'https://goal-setter-eta.vercel.app'}));
+app.use(cors({credentials:true,origin: 'https://goal-setter-eta.vercel.app/login'}));
 
 //middleware so that we can access the req.body in POST reqs
 app.use(express.json());
