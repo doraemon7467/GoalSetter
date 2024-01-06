944ea77da05d2b3a25da396b9e51ef67bbb263cf
Aayu <106157824+doraemon7467@users.noreply.github.com>
2024-01-06 15:08:57 +0530

    connection db

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,8 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI)
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+    // console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+    console.log("Database Connected",conn.connection.host, conn.connection.name)
   } catch (error) {
     console.log(error)
     process.exit(1)
