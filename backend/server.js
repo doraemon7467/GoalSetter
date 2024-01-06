7bc517659c4d05cf9056c799f06876fa08b3bbe7
Aayu <106157824+doraemon7467@users.noreply.github.com>
2024-01-06 16:31:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 const corsOptions = {
-    origin: 'https://goal-setter-eta.vercel.app/login',
+    origin: 'https://goal-setter-eta.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true, // If your application requires cookies or authorization headers
   };
