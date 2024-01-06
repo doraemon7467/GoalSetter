7d32ed4c69bdd538e0fa099a6f856b469dbd83d8
Aayu <106157824+doraemon7467@users.noreply.github.com>
2024-01-06 15:15:34 +0530

    path change

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin : ["https://localhost:3000"],
+    origin : ["https://goal-setter-eta.vercel.app/login"],
   })
 )
 
