2a3b8a8e9c0e83f799ac4504406b608ec16a186c
Aayu <106157824+doraemon7467@users.noreply.github.com>
2024-01-07 19:40:25 +0530

    comments

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,8 +7,11 @@ const User = require('../models/userModel')
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
+
+  // Destructures name, email, and password from the req.body.
   const { name, email, password } = req.body
 
+  // Check if all fields are available
   if (!name || !email || !password) {
     res.status(400)
     throw new Error('Please add all fields')
@@ -23,7 +26,9 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   // Hash password
+  // Generates a salt (a random string) for password hashing.
   const salt = await bcrypt.genSalt(10)
+  // Hashes the user's password using the generated salt 
   const hashedPassword = await bcrypt.hash(password, salt)
 
   // Create user
@@ -50,11 +55,15 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
+
+  // Destructures email and password from the req.body
   const { email, password } = req.body
 
   // Check for user email
   const user = await User.findOne({ email })
 
+  // Checks if a user with the provided email exists
+  // and if the provided password matches the hashed password stored in the database for that user.
   if (user && (await bcrypt.compare(password, user.password))) {
     res.json({
       _id: user.id,
