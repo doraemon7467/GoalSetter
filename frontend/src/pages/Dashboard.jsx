12afabc1d8034d7f52e0f5a0a309bb3a01eb28c4
Aayu <106157824+doraemon7467@users.noreply.github.com>
2024-01-09 17:06:28 +0530

    comments

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -5,18 +5,29 @@ import GoalForm from '../components/GoalForm'
 import GoalItem from '../components/GoalItem'
 import Spinner from '../components/Spinner'
 import Popup from '../components/Popup'
-import { getGoals, reset } from '../features/goals/goalSlice'
+import { getGoals, reset } from '../features/goals/goalSlice' // Importing action creators
 import { toast } from 'react-toastify'
 
+// How much time has passed since the goal is created
 function getAge(createdAt) {
+  // Calculate the difference in milliseconds between the current date and the createdAt date
   const msDiff2 = new Date().getTime() - new Date(createdAt).getTime();    //current date - created date
+
+  // Convert the time difference from milliseconds to days
   const age = Math.floor(msDiff2 / (1000 * 60 * 60 * 24)) ;
+
+  // Return the age in days
   return age;
 }
 
 function getDaysLeft(completeTime){
+  // Calculate the difference in milliseconds between the future date (completeTime) and the current date
   const msDiff1 = new Date(completeTime).getTime() - new Date().getTime();    //Future date - current date
+
+  // Convert the time difference from milliseconds to days
   const daysLeft = Math.floor(msDiff1 / (1000 * 60 * 60 * 24)) + 1;
+
+  // Return the number of days left
   return daysLeft;
 }
 
@@ -25,14 +36,21 @@ function getAgedPriority(completeTime, originalPriority){
   // const currentDate = new Date();
   // const deadline = new Date(completeTime);
   const timeRemaining = getDaysLeft(completeTime);
+
+  // If the time remaining is negative (past the completion time), return a default value (-1)
   if(timeRemaining < 0) return -1;
 
   const priorityIncreaseFactor = 1;
 
+
+ // Calculate the updated priority based on an exponential formula
   const updatedPriority = (originalPriority * Math.exp(1 / (priorityIncreaseFactor * timeRemaining))).toFixed(2);
 
   // Return the updated priority with a maximum of 100.
   // return Math.min(updatedPriority, 100);
+
+  // Return the updated priority value considering the original priority and the aging factor
+  
   return Math.max(updatedPriority, originalPriority);
 }
 
@@ -41,27 +59,34 @@ function Dashboard() {
   const navigate = useNavigate()
   const dispatch = useDispatch()
 
+  // State for managing popup visibility and sorting parameters
   const [buttonPopup, setButtonPopup] = useState(false);
   const [sortParameter, setSortParameter] = useState(null)
   
+  // Retrieving data from Redux store
   const { user } = useSelector((state) => state.auth)
   const { goals, isLoading, isError, message } = useSelector((state) => state.goals)
 
   // object = { ...object, property1: value1, property2: value2 }
   
+  // State to manage sorted goals
   const [sortedGoals, setSortedGoals] = useState(goals);
 
+  // useEffect hook to handle initial loading and redirection
   useEffect(() => {
 
+    // Redirect to login if user is not authenticated
     if (!user) {
       navigate('/login')
     }
     
+    // Display error toast if there's an error fetching goals
     if (isError) {
       if(message !== "Cannot read properties of null (reading 'token')")
         toast.error(message);
     }
 
+    // Dispatch action to fetch goals
     dispatch(getGoals())
 
 
@@ -69,6 +94,7 @@ function Dashboard() {
  
   useEffect(() => {
 
+    // useEffect to sort goals based on sortParameter change
     setSortedGoals(goals.slice().sort((a,b) => {
       if(sortParameter === "completeTime"){
         return (new Date(a[sortParameter]) - new Date(b[sortParameter]));
@@ -90,10 +116,12 @@ function Dashboard() {
   }, [sortParameter, goals]);
 
 
+  // If data is loading, display a Spinner component
   if (isLoading) {
     return <Spinner />
   }
 
+  // JSX for rendering the Dashboard component
   return (
     <>
       <section className='heading'>
@@ -101,8 +129,9 @@ function Dashboard() {
         <p>Goals Dashboard</p>
       </section>
 
+       {/* Dropdown for selecting sorting parameter */}
       <section className="sort">
-        <label for="sort" className='sortLabel'>Sort on the basis of </label>
+        <label htmlfor="sort" className='sortLabel'>Sort on the basis of </label>
         <select className="dropdown" name="sortParam" value={sortParameter} onChange={(e) => {setSortParameter(e.target.value)}}>
           <option value="createdAt">Create Time</option>
           <option value="priority">Priority</option>
@@ -113,6 +142,7 @@ function Dashboard() {
         </select>
       </section>
 
+      {/* Rendering goals */}
       <section className='content'>
         {sortedGoals.length > 0 ? (
           <div className='goals'>
@@ -131,8 +161,10 @@ function Dashboard() {
       <br></br>
       <br></br>
 
+       {/* Button to trigger goal form popup */}
       <button onClick={() => setButtonPopup(true)} className='btn btn-trigger'> Add a Goal </button>
 
+      {/* Popup component for adding a goal */}
       <Popup trigger={buttonPopup} setTrigger={setButtonPopup}> <GoalForm setTrigger={setButtonPopup}/> </Popup>
 
       {/* <GoalForm /> */}
@@ -140,4 +172,4 @@ function Dashboard() {
   )
 }
 
-export default Dashboard
+export default Dashboard  // Exporting the Dashboard component
